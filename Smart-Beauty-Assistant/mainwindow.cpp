1cbe0f90110ce233fe43d8b491794a8d6873e137
EvLast <3178146280@qq.com>
2024-10-11 15:46:02 +0800

    更新架构完成桌面宠物基本模块

--- Smart-Beauty-Assistant/mainwindow.cpp
+++ Smart-Beauty-Assistant/mainwindow.cpp
@@ -1,5 +1,6 @@
  #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <string>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,6 +8,16 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    //设置窗口图标
+    this->setWindowIcon(QIcon(":/sound/other/btn1.png"));
+    //去除边框
+    this->setWindowFlags(Qt::FramelessWindowHint);
+
+    //背景透明
+    //this->setWindowOpacity(1);
+    this->setAttribute(Qt::WA_TranslucentBackground);
+
+    // 将窗口固定大小
     QBitmap bmp(this->size());//设置圆角边框
     bmp.fill();
     QPainter p(&bmp);
@@ -15,15 +26,35 @@ MainWindow::MainWindow(QWidget *parent)
     p.drawRoundedRect(bmp.rect(),50,50);
     setMask(bmp);
 
-    //设置窗口图标
-    this->setWindowIcon(QIcon(":/sound/other/btn1.png"));
-    //去除边框
-    this->setWindowFlags(Qt::FramelessWindowHint);
-    //背景透明
-    this->setWindowOpacity(1);
-    this->setAttribute(Qt::WA_TranslucentBackground);
-    //设置窗口顶层
+
+    //初始化定时器
+    timer = new QTimer(this);
+    //加载初始动作
+    loadRoleActRes();
+
+    //创建信号与槽
+    connect(timer, &QTimer::timeout,[this](){
+        static int index = 0; //记录显示动作的当前图片索引
+        auto paths = this->action_map.value(this->cur_role_act);
+
+        this->cur_role_pix = paths[index++ % paths.size()];
+
+        this->update();
+    } );
+
+    showActAnimation(Act::Blink);
+
+
+    /*设置窗口顶层让图标消失
     this->setWindowFlags(this->windowFlags() |Qt::Tool);
+    */
+
+    /*  老版设置定时器
+        timer = new QTimer(this);
+        connect(timer,&QTimer::timeout,this,&MainWindow::showMovie);
+        timer->start(500);
+    */
+
 }
 
 MainWindow::~MainWindow()
@@ -40,12 +71,14 @@ void MainWindow::mouseMoveEvent(QMouseEvent *event)
 {
     //鼠标右键 与 点击
     if (event->type() == QEvent::MouseMove) {
-        qDebug() << "Mouse Move Event:" << event->pos();
+        //qDebug() << "Mouse Move Event:" << event->pos();
+
         //移动窗口
         /*
          *  this->current_pos = QPoint(e->globalPos()-m_pos);
          *  this->move(e->globalPos()-this->m_pos);
         */
+
         pix.load(":/sound/img/shime111.png");
         this->current_pos = QPoint(event->globalPosition().toPoint() - m_pos);
         this->move(event->globalPosition().toPoint()-this->m_pos);
@@ -61,7 +94,7 @@ void MainWindow::mouseMoveEvent(QMouseEvent *event)
 void MainWindow::mousePressEvent(QMouseEvent *event)
 {
     //鼠标按下记录分量
-    m_pos = event->globalPos()-this->pos();
+    m_pos = event->globalPosition().toPoint()-this->pos();
 }
 
 /*
@@ -71,57 +104,103 @@ void MainWindow::mousePressEvent(QMouseEvent *event)
 */
 void MainWindow::enterEvent(QEvent *event)
 {
-
+    qDebug() << "Mouse Move Event:" ;
 }
 
+// /*
+//     功能： 动画显示
+//     参数：void
+//     返回值：void
+// */
+// void MainWindow::showMovie()
+// {
+
+// }
+
+// /*
+//     功能： 动画显示
+//     参数：void
+//     返回值：void
+// */
+// void MainWindow::showMovie1()
+// {
+
+// }
+
+// /*
+//     功能： 动画显示
+//     参数：void
+//     返回值：void
+// */
+// void MainWindow::showMovie2()
+// {
+
+// }
+
+// /*
+//     功能： 动画显示
+//     参数：void
+//     返回值：void
+// */
+// void MainWindow::showMovie3()
+// {
+
+// }
+
+// /*
+//     功能： 动画显示
+//     参数：void
+//     返回值：void
+// */
+// void MainWindow::showMovie3Move()
+// {
+
+// }
+
 /*
-    功能： 动画显示
+    功能： 加载图片, 将动作映射到Map中
     参数：void
     返回值：void
 */
-void MainWindow::showMovie()
+void MainWindow::loadRoleActRes()
 {
-
+    /*
+        功能： 用lambda表达式加载图片, 将动作映射到Map中
+        参数：Act::RoleAct act,QString path, int count
+            - Act::RoleAct act 传递动作类型
+            - QString path 传递图片地址
+            - int count 传递图片数量
+        返回值：void
+    */
+    auto addRes = [this](Act::RoleAct act,QString path, int count) {
+        QList<QUrl> paths;
+        //算法训练之字符串拼接
+        for (int i = 1; i <= count; ++i) {
+            QString buf(path);
+            buf += QString::fromStdString(std::to_string(i));
+            buf += QString::fromStdString(".png");
+            //QUrl::fromLocalFile(buf) 将本地文件转换成QUrl类型
+            paths.append(QUrl::fromLocalFile(buf));
+            qDebug() << buf;
+        }
+        //完成动作与素材间的映射
+        action_map.insert(act, paths);
+    };
+    addRes(Act::Blink,":/sound/img/shime11",3);
 }
 
 /*
-    功能： 动画显示
+    功能： 定时器更新
     参数：void
     返回值：void
 */
-void MainWindow::showMovie1()
+void MainWindow::showActAnimation(Act::RoleAct act)
 {
+    timer->stop();
 
-}
+    this->cur_role_act = act;
 
-/*
-    功能： 动画显示
-    参数：void
-    返回值：void
-*/
-void MainWindow::showMovie2()
-{
-
-}
-
-/*
-    功能： 动画显示
-    参数：void
-    返回值：void
-*/
-void MainWindow::showMovie3()
-{
-
-}
-
-/*
-    功能： 动画显示
-    参数：void
-    返回值：void
-*/
-void MainWindow::showMovie3Move()
-{
-
+    timer->start(120);
 }
 
 /*
@@ -129,9 +208,11 @@ void MainWindow::showMovie3Move()
     参数：void
     返回值：void
 */
-void MainWindow::paintEvent(QPaintEvent *)
+void MainWindow::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
-    pix.load(":/sound/img/shime111.png");
+    //从URl转换为本地图片
+    pix.load(this->cur_role_pix.toLocalFile());
+    //绘制图片
     painter.drawPixmap(0,0,pix);
 }
--- Smart-Beauty-Assistant/mainwindow.h
+++ Smart-Beauty-Assistant/mainwindow.h
@@ -6,11 +6,34 @@
 #include <QPixmap>
 #include <QMouseEvent>
 #include <qDebug>
+#include <QTimer>
+#include <QMap>
+#include <QUrl>
+#include <QList>
 
 QT_BEGIN_NAMESPACE
+
 namespace Ui {
 class MainWindow;
 }
+
+namespace Act{
+
+//注册枚举命名空间
+Q_NAMESPACE
+
+//枚举动作类型
+enum RoleAct{
+        Sleep,
+        Drowse, // 昏昏欲睡
+        Work,  //走路
+        Drag, //拖拽 , 鼠标滑动
+        Blink // 眨眼睛
+    };
+    Q_ENUM_NS_IMPL(RoleAct)
+}
+
+
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -28,16 +51,20 @@ public:
     void enterEvent(QEvent *event);
 
 public slots:
-    //显示眨眼动画
-    void showMovie();
-    //显示动画1 打瞌睡
-    void showMovie1();
-    //显示动画2 睡觉
-    void showMovie2();
-    //显示动画3 走路
-    void showMovie3();
-    //显示动画4 拖拽
-    void showMovie3Move();
+    // //显示眨眼动画
+    // void showMovie();
+    // //显示动画1 打瞌睡
+    // void showMovie1();
+    // //显示动画2 睡觉
+    // void showMovie2();
+    // //显示动画3 走路
+    // void showMovie3();
+    // //显示动画4 拖拽
+    // void showMovie3Move();
+    //加载动画
+    void loadRoleActRes();
+    // 展示Animation(动画)
+    void showActAnimation(Act::RoleAct);
 
 private:
     Ui::MainWindow *ui;
@@ -48,15 +75,29 @@ private:
     //窗口目前的位置
     QPoint current_pos;
 
-    //眨眼动画的定时器
+    //动画的定时器
     QTimer *timer;
 
-    //存放图片
+    //新型存放图片的形式
+    QMap<Act::RoleAct,QList<QUrl>> action_map;
+
+    //记录动作类型: 建立映射关系
+    Act::RoleAct cur_role_act;
+    //记录当前所用动作
+    QUrl cur_role_pix;
+
+    //存放图片后续更改图片更方便
     QPixmap pix;
 
     //绘图事件
     void paintEvent(QPaintEvent *);
 
+    //动画地址
+    QString pic_adrress;
+    QString pic_adrress_1 = ":/sound/img/shime11";
+    QString pic_adrress_2 = ":/sound/img/shime22";
+    QString pic_adrress_3 = ":/sound/img/shime33";
+    QString pic_adrress_4 = ":/sound/img/shime44";
 
 };
 #endif // MAINWINDOW_H
